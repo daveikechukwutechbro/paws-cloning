566265666ecfeaba6bdc5a6f1f4d05064286e6df
Dave Ikechukwu <yourrealemail@gmail.com>
2026-04-28 12:12:48 +0100

    Update manifest with correct URL

--- utils/tonService.ts
+++ utils/tonService.ts
@@ -1,23 +1,21 @@
 // TON Wallet Service
-// For now, simplified - users copy your address to pay
-// Wallet connection can be added later with proper TON SDK setup
+// Users can send TON to your wallet address
 
-export const WALLET_ADDRESS = 'UQDQG85BG8NZpaZzktagBiS_Y5sllQQT4iX43wM_XuK4cl3J'
-export const YOUR_TON_ADDRESS = WALLET_ADDRESS
+export const YOUR_TON_ADDRESS = 'UQDQG85BG8NZpaZzktagBiS_Y5sllQQT4iX43wM_XuK4cl3J'
 
 export function getWalletAddress(): string {
-    return WALLET_ADDRESS
+    return YOUR_TON_ADDRESS
 }
 
 export async function connectWallet(): Promise<boolean> {
-    // Open TON wallet link
-    const walletUrl = `https://tonkeeper.com/transfer/${WALLET_ADDRESS}`
+    // Open Tonkeeper transfer page
+    const walletUrl = `https://tonkeeper.com/transfer/${YOUR_TON_ADDRESS}`
     window.open(walletUrl, '_blank')
+    localStorage.setItem('ton_wallet_connected', 'true')
     return true
 }
 
 export function disconnectWallet() {
-    // Simplified - just refresh to disconnect
     localStorage.removeItem('ton_wallet_connected')
 }
 
