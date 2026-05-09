53d40d6d5dd82684d1e96c57175e4ccdaa88e3e4
Dave Ikechukwu <yourrealemail@gmail.com>
2026-05-09 13:27:20 +0100

    fix: remove missing image imports from index.ts

--- images/index.ts
+++ images/index.ts
@@ -4,9 +4,6 @@ import paws from "./paws.webp"
 import taskWhitePaws from "./task-white-paws.webp"
 import taskBoost from "./task-boost.webp"
 import taskBlum from "./task-blum.webp"
-import taskVideo from "./task-video.webp"
-import taskImage from "./task-image.webp"
-import taskSound from "./task-sound.webp"
 
 export {
     sparkles,
@@ -14,8 +11,5 @@ export {
     paws,
     taskWhitePaws,
     taskBoost,
-    taskBlum,
-    taskVideo,
-    taskImage,
-    taskSound
+    taskBlum
 }
